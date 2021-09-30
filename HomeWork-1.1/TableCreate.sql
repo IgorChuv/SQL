3154c353b030b8f6633705ae8319527f3a442991
Igor Chuvilov <igorchuv74@gmail.com>
2021-09-30 14:11:34 +0500

    HomeWork-1.1.

---
+++ HomeWork-1.1/TableCreate.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE MY_TABLES;
+USE MY_TABLES;
+
+CREATE TABLE PERSONS(
+                        name varchar(30) DEFAULT 'UNKNOWN',
+                        surname varchar(30) DEFAULT 'UNKNOWN',
+                        age int CHECK (age > 0),
+                        phone_number varchar(30) DEFAULT 'UNKNOWN',
+                        city_of_living varchar(30) DEFAULT 'UNKNOWN',
+                        PRIMARY KEY (name,surname,age)
+);
